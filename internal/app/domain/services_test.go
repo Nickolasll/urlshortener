a749cd738437c78543e63862431235c9589a4355
Nikolay Magritov <Nikolay.Magritov@bki-okb.ru>
2023-09-04 00:29:33 +0300

    [Инкремент 2] - Добавлены unit тесты

---
+++ internal/app/domain/services_test.go
@@ -0,0 +1,39 @@
+package domain
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateSlug(t *testing.T) {
+	type args struct {
+		size int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "zero size",
+			args: args{
+				size: 0,
+			},
+			want: 1,
+		},
+		{
+			name: "simple success",
+			args: args{
+				size: 5,
+			},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := GenerateSlug(tt.args.size)
+			assert.Equal(t, len(got), tt.want)
+		})
+	}
+}
---
+++ internal/app/presentation/views_test.go
@@ -0,0 +1,76 @@
+package presentation
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_mainPage(t *testing.T) {
+	type want struct {
+		contentType string
+		statusCode  int
+		body        string
+	}
+	tests := []struct {
+		name   string
+		body   string
+		method string
+		want   want
+	}{
+		{
+			name:   "success short and unshort",
+			body:   "https://practicum.yandex.ru/",
+			method: http.MethodPost,
+			want: want{
+				statusCode:  201,
+				contentType: "text/plain",
+				body:        "http://example.com/ImePAqgP",
+			},
+		},
+		{
+			name:   "bad request",
+			body:   "https://practicum.yandex.ru/",
+			method: http.MethodPut,
+			want: want{
+				statusCode:  400,
+				contentType: "",
+				body:        "",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bodyReader := strings.NewReader(tt.body)
+			request := httptest.NewRequest(tt.method, "/", bodyReader)
+			recorder := httptest.NewRecorder()
+			handler := http.HandlerFunc(mainPage)
+			handler(recorder, request)
+
+			result := recorder.Result()
+
+			assert.Equal(t, tt.want.statusCode, result.StatusCode)
+			assert.Equal(t, tt.want.contentType, result.Header.Get("Content-Type"))
+			defer result.Body.Close()
+			resBody, err := io.ReadAll(result.Body)
+			require.NoError(t, err)
+			assert.Equal(t, len(tt.want.body), len(resBody))
+			if tt.want.statusCode == 201 {
+				bodyReader := strings.NewReader(tt.body)
+				request := httptest.NewRequest(http.MethodGet, string(resBody), bodyReader)
+				recorder := httptest.NewRecorder()
+				handler := http.HandlerFunc(mainPage)
+				handler(recorder, request)
+
+				result := recorder.Result()
+				assert.Equal(t, http.StatusTemporaryRedirect, result.StatusCode)
+				assert.Equal(t, tt.body, result.Header.Get("Location"))
+			}
+		})
+	}
+}

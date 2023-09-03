7bf4484e100051a8faa6f730d75ab63bd4217cae
Nikolay Magritov <Nikolay.Magritov@bki-okb.ru>
2023-09-03 22:34:22 +0300

    refactoring

--- internal/app/presentation/views.go
+++ internal/app/presentation/views.go
@@ -1,30 +1,9 @@
 package presentation
 
-import (
-	"io"
-	"net/http"
-
-	"github.com/Nickolasll/urlshortener/internal/app/domain"
-	"github.com/Nickolasll/urlshortener/internal/app/infrastructure"
-)
+import "net/http"
 
 func mainPage(res http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		body, _ := io.ReadAll(req.Body)
-		slug := domain.GenerateSlug(8)
-		infrastructure.RAMRepository.Save(string(body), slug)
-		res.Header().Set("content-type", "text/plain")
-		res.WriteHeader(http.StatusCreated)
-		res.Write([]byte("http://" + req.Host + slug))
-		return
-	} else if req.Method == http.MethodGet {
-		slug := req.URL.Path
-		value, ok := infrastructure.RAMRepository.Get(slug)
-		if ok {
-			res.Header().Add("Location", value)
-			res.WriteHeader(http.StatusTemporaryRedirect)
-			return
-		}
-	}
+	PostHandler(res, req)
+	GetHandler(res, req)
 	res.WriteHeader(http.StatusBadRequest)
 }

a5241440763e181081ec4a2c9b92e0d2068343b8
Nikolay Magritov <Nikolay.Magritov@bki-okb.ru>
2023-10-17 16:56:21 +0300

    review

--- internal/app/presentation/handlers.go
+++ internal/app/presentation/handlers.go
@@ -28,91 +28,76 @@ type BatchOutput struct {
 }
 
 func GetHandler(res http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		slug := req.URL.Path
-		value, _ := repository.GetOriginalURL(slug)
-		if value != "" {
-			res.Header().Add("Location", value)
-			res.WriteHeader(http.StatusTemporaryRedirect)
-			return
-		}
+	slug := req.URL.Path
+	value, _ := repository.GetOriginalURL(slug)
+	if value != "" {
+		res.Header().Add("Location", value)
+		res.WriteHeader(http.StatusTemporaryRedirect)
 	}
 }
 
 func PostHandler(res http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		res.Header().Set("content-type", "text/plain")
-		body, _ := io.ReadAll(req.Body)
-		short := domain.Shorten(string(body))
-		err := repository.Save(short)
-		if err != nil {
-			slug, _ := repository.GetShortURL(short.OriginalURL)
-			res.WriteHeader(http.StatusConflict)
-			res.Write([]byte(*config.SlugEndpoint + slug))
-			return
-		}
-		res.WriteHeader(http.StatusCreated)
-		res.Write([]byte(*config.SlugEndpoint + short.ShortURL))
+	res.Header().Set("content-type", "text/plain")
+	body, _ := io.ReadAll(req.Body)
+	short := domain.Shorten(string(body))
+	err := repository.Save(short)
+	if err != nil {
+		slug, _ := repository.GetShortURL(short.OriginalURL)
+		res.WriteHeader(http.StatusConflict)
+		res.Write([]byte(*config.SlugEndpoint + slug))
 		return
 	}
+	res.WriteHeader(http.StatusCreated)
+	res.Write([]byte(*config.SlugEndpoint + short.ShortURL))
 }
 
 func ShortenHandler(res http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		res.Header().Set("Content-Type", "application/json")
-		body, _ := io.ReadAll(req.Body)
-		var input Input
-		json.Unmarshal(body, &input)
-		if input.URL == "" {
-			res.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		short := domain.Shorten(input.URL)
-		err := repository.Save(short)
-		if err != nil {
-			slug, _ := repository.GetShortURL(short.OriginalURL)
-			resp, _ := json.Marshal(Output{Result: *config.SlugEndpoint + slug})
-			res.WriteHeader(http.StatusConflict)
-			res.Write(resp)
-			return
-		}
-		resp, _ := json.Marshal(Output{Result: *config.SlugEndpoint + short.ShortURL})
-		res.WriteHeader(http.StatusCreated)
+	res.Header().Set("Content-Type", "application/json")
+	body, _ := io.ReadAll(req.Body)
+	var input Input
+	json.Unmarshal(body, &input)
+	if input.URL == "" {
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	short := domain.Shorten(input.URL)
+	err := repository.Save(short)
+	if err != nil {
+		slug, _ := repository.GetShortURL(short.OriginalURL)
+		resp, _ := json.Marshal(Output{Result: *config.SlugEndpoint + slug})
+		res.WriteHeader(http.StatusConflict)
 		res.Write(resp)
 		return
 	}
+	resp, _ := json.Marshal(Output{Result: *config.SlugEndpoint + short.ShortURL})
+	res.WriteHeader(http.StatusCreated)
+	res.Write(resp)
 }
 
 func PingHandler(res http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		if repository.Ping() != nil {
-			res.WriteHeader(http.StatusInternalServerError)
-			return
-		}
+	if repository.Ping() != nil {
+		res.WriteHeader(http.StatusInternalServerError)
 	}
 }
 
 func BatchShortenHandler(res http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		res.Header().Set("Content-Type", "application/json")
-		body, _ := io.ReadAll(req.Body)
-		var batchInput []BatchInput
-		json.Unmarshal(body, &batchInput)
-		shorts := []domain.Short{}
-		batchOutput := []BatchOutput{}
-		for _, batch := range batchInput {
-			short := domain.Shorten(batch.OriginalURL)
-			shorts = append(shorts, short)
-			output := BatchOutput{
-				CorrelationID: batch.CorrelationID,
-				ShortURL:      *config.SlugEndpoint + short.ShortURL,
-			}
-			batchOutput = append(batchOutput, output)
+	res.Header().Set("Content-Type", "application/json")
+	body, _ := io.ReadAll(req.Body)
+	var batchInput []BatchInput
+	json.Unmarshal(body, &batchInput)
+	shorts := []domain.Short{}
+	batchOutput := []BatchOutput{}
+	for _, batch := range batchInput {
+		short := domain.Shorten(batch.OriginalURL)
+		shorts = append(shorts, short)
+		output := BatchOutput{
+			CorrelationID: batch.CorrelationID,
+			ShortURL:      *config.SlugEndpoint + short.ShortURL,
 		}
-		repository.BulkSave(shorts)
-		resp, _ := json.Marshal(batchOutput)
-		res.WriteHeader(http.StatusCreated)
-		res.Write(resp)
-		return
+		batchOutput = append(batchOutput, output)
 	}
+	repository.BulkSave(shorts)
+	resp, _ := json.Marshal(batchOutput)
+	res.WriteHeader(http.StatusCreated)
+	res.Write(resp)
 }
--- internal/app/presentation/views.go
+++ internal/app/presentation/views.go
@@ -3,7 +3,11 @@ package presentation
 import "net/http"
 
 func mainPage(res http.ResponseWriter, req *http.Request) {
-	PostHandler(res, req)
-	GetHandler(res, req)
-	res.WriteHeader(http.StatusBadRequest)
+	if req.Method == http.MethodPost {
+		PostHandler(res, req)
+	} else if req.Method == http.MethodGet {
+		GetHandler(res, req)
+	} else {
+		res.WriteHeader(http.StatusBadRequest)
+	}
 }
